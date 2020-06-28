3c2d7f2c542ff08c3ed96d2a9e7aaeacbebbac72
Sergey <zavulon31990@gmail.com>
2020-06-28 16:46:53 +0300

    5 экран

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ $('.header_menu').on('click', function (e) {
     $(this).toggleClass('active');
     $('.navList').toggleClass('active_menu');
 });
+var point = document.querySelectorAll('.point');
+
+console.log(point);
